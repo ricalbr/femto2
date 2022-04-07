86329ab3a82805ba030d8f4c5d31021c44fae6e5
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-04-07 18:57:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ setup(
     description='Python suite for the design of femtosecond-written circuit.',
     version='1.0',
     packages=find_packages(include=['flww',
-                                    'flww.*']),
+                                    'flww.*',
+                                    'compiler',
+                                    'compiler.*',
+                                    'objects',
+                                    'objects.*']),
     install_requires=[
         'pandas',
         'numpy',
