Fix rotation matrix

flww/compiler/PGMCompiler.py
@@ -3,20 +3,21 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 class PGMCompiler:
-    def __init__(self, filename, line='CAPABLE', rotation_angle=0.0, n_rif=None, long_pause=0.5, short_pause=0.15):
+    def __init__(self, filename, line='CAPABLE', angle=0.0, n_rif=None, long_pause=0.5, short_pause=0.15):
         
         self._filename = filename
         self._line=line
         self._long_pause=long_pause
         self._short_pause=short_pause
-        self._rotation_angle=rotation_angle
+        self._angle=angle
         self._n_rif=n_rif
-        self._RM = np.array([[1,0,0], 
-                             [0, np.cos(self._rotation_angle), -np.sin(self._rotation_angle)],
-                             [0, np.sin(self._rotation_angle), np.cos(self._rotation_angle)]])
+
+        self._RM = np.array([[np.cos(self._angle), -np.sin(self._angle), 0],
+                             [np.sin(self._angle), np.cos(self._angle), 0],
+                             [0, 0, 1]])
         self._SM = np.array([[1,0,0],
-                             [0,1,0],
-                             [0,0,1/self._n_rif]])
+                              [0,1,0],
+                              [0,0,1/self._n_rif]])
         self._instructions = ''
         
     def header(self):
@@ -86,7 +87,8 @@ class PGMCompiler:
                 shutter_on = True
             else:
                 self._instructions += f'LINEAR X{x[i]:.6f} Y{y[i]:.6f} Z{z[i]:.6f} F{f[i]:.6f}\n'
-            
+        return (x,y,z,f,s)
+    
     def compile_pgm(self):
         f = open(self._filename, "w")
         f.write(self._instructions)
@@ -103,7 +105,7 @@ if __name__ == '__main__':
     depth = 0.035
     int_dist = 0.007
     int_length = 0.0
-    tilt_angle = 0.1
+    angle = np.radians(45)
     tot_length = 25
     length_arm = 1.5
     
@@ -112,7 +114,7 @@ if __name__ == '__main__':
     increment = [Dx, Dy, Dz]
     
     # Calculations
-    coup = [Waveguide(num_scan=6) for _ in range(2)]
+    coup = [Waveguide(num_scan=6) for _ in range(20)]
     for index, wg in enumerate(coup):
         [xi, yi, zi] = [-2, -pitch/2 + index*pitch, depth]
         
@@ -121,9 +123,9 @@ if __name__ == '__main__':
         wg.mzi_sin((-1)**index*d_bend, radius, length_arm, speed,)
         wg.linear(increment, speed)
         wg.end()
-        
+    
     # Compilation 
-    gc = PGMCompiler('test.pgm', n_rif=1.5/1.33)
+    gc = PGMCompiler('PROVA.pgm', n_rif=1.5/1.33, angle=angle)
     gc.header()
     gc.rpt(wg.num_scan)
     for i, wg in enumerate(coup):    