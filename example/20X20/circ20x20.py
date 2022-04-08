fe0bd94eba0b82179552fea74d67e6bc4f5247da
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-04-08 18:07:11 +0200

    Add 20x20 example

---
+++ example/20X20/circ20x20.py
@@ -0,0 +1,43 @@
+from flww.objects.Waveguide import Waveguide
+from flww.compiler.PGMCompiler import PGMCompiler
+import param as p
+import matplotlib.pyplot as plt
+import time 
+t = time.time()
+
+# 20x20 circuit
+circ20x20 = [Waveguide() for _ in range(p.MM)]
+increment = [p.swg_length, 0.0, 0.0]
+
+for i, wg in enumerate(circ20x20):
+    [xi, yi, zi] = [p.x0, p.y0-(i-0.5*(p.MM+1))*p.pitch, p.depth]
+    
+    wg.start([xi, yi, zi])
+    wg.linear(increment, p.speed)
+    wg.sin_bend((-1)**(i%2+1)*p.d1, p.radius, p.speed)
+    for j in range(p.NN-1):
+        wg.acc_sin((-1)**(j+i%2)*p.d1, p.radius, speed=p.speed)
+        wg.sin_bend((-1)**(j+i%2)*p.d2, p.radius, p.speed)
+    wg.sin_bend((-1)**(j+i%2+1)*p.d1, p.radius, p.speed)    
+    wg.acc_sin((-1)**(j+i%2)*p.d1, p.radius, speed=p.speed)
+    wg.linear(increment, p.speed)
+    wg.end()
+
+# Plot
+fig, ax = plt.subplots()
+for wg in circ20x20:
+    ax.plot(wg.M['x'][:-1], wg.M['y'][:-1], '-b', linewidth=2.5)
+    ax.plot(wg.M['x'][-2:], wg.M['y'][-2:], ':k', linewidth=1)
+plt.show()
+
+# Compilation 
+gc = PGMCompiler('20x20cirq.pgm', ind_rif=p.ind_env, angle=p.angle)
+gc.header()
+gc.rpt(wg.num_scan)
+for i, wg in enumerate(circ20x20):    
+    gc.comment(f'Modo: {i}')
+    gc.point_to_instruction(wg.M)
+gc.endrpt()
+gc.compile_pgm()
+
+print(f'Elapsed time: {time.time() - t:.2f} s.')
---
+++ example/20X20/param.py
@@ -0,0 +1,29 @@
+import math
+
+# %% GEOMETRICAL DATA
+MM = 20
+NN = 20
+
+radius = 15
+pitch = 0.080
+pitch_fa = 0.127
+depth = 0.035
+int_distance = 0.007
+int_length = 0.0
+length_arm = 0.0
+speed = 20
+swg_length = 3
+
+x0 = -2.0
+y0 = 0.0
+z0 = depth
+
+d1 = 0.5*(pitch-int_distance)
+d2 = pitch-int_distance
+
+# %% G-CODE DATA
+n_scan = 6
+ind_glass = 1.5
+ind_water = 1.33
+ind_env = ind_glass/ind_water
+angle = math.radians(1.35)
