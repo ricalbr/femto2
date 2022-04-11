d9bb0d91178a5f08fb073192daaab7ded7e61359
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-04-11 18:23:18 +0200

    Add PGM method
    Numerous methods have been added:
    - shutter(): method to open and close the shutter
    - dwell(): method to add pauses
    - set_home(): to use the G92 command
    - homing(): to return to (0,0,0)
    - move_to(): to make SHUTTER CLOSED movements
    
    Moreover, several other methods have been improved. The header() method is now much more concise, and it copies the instructions (which are fixed for every .pgm file) from dedicated text files. A function that formats the coordinates has been added and used in the point_to_instruction() method to make it more readable.
    
    The example script has been simplified a lot as well as the class itself. A lot of attributes are now public (without the need of dedicated getters and setters).
    
    Finally, additional attributes are introduced to track some states of the program:
    - total_dwell_time: is the sum of the time spent in dwell operation (can be used to compute the total duration of a fabrication)
    - _shutter_on: tracks the state of the shutter during the fabrication

--- flww/compiler/PGMCompiler.py
+++ flww/compiler/PGMCompiler.py
@@ -1,32 +1,30 @@
 from flww.objects.Waveguide import Waveguide
 import numpy as np 
-import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+CWD = os.path.dirname(os.path.abspath(__file__))
 
 class PGMCompiler:
-    def __init__(self, filename, ind_rif, fab_line='CAPABLE', angle=0.0, long_pause=0.5, short_pause=0.15):
+    def __init__(self, filename, ind_rif, fab_line='CAPABLE', angle=0.0, long_pause=0.5, short_pause=0.15, output_digits=6):
+    
+        self.filename = filename
+        self.fab_line=fab_line
+        self.long_pause=long_pause
+        self.short_pause=short_pause
         
-        self._filename = filename
+        self.output_digits=output_digits
+        self.total_dwell_time=0.0
+        
+        self.TM = None
         self._ind_rif=ind_rif
-        self._fab_line=fab_line
         self._angle=angle
-        self._long_pause=long_pause
-        self._short_pause=short_pause
+        self._compute_t_matrix()
         
-        self._TM = None
-        self.compute_t_matrix()
-        
+        self._shutter_on = False
         self._instructions = []
         
     # Getters/Setters
-    @property
-    def filename(self):
-        return self._filename
-    
-    @filename.setter
-    def filename(self, filename):
-        self._filename=filename
-        
     @property
     def ind_rif(self):
         return self._ind_rif
@@ -34,15 +32,7 @@ class PGMCompiler:
     @ind_rif.setter
     def ind_rif(self, ind_rif):
         self._ind_rif=ind_rif
-        self.compute_t_matrix()
-        
-    @property
-    def fab_line(self):
-        return self._fab_line
-    
-    @fab_line.setter
-    def fab_line(self, fab_line):
-        self._fab_line=fab_line
+        self._compute_t_matrix()
         
     @property
     def angle(self):
@@ -51,103 +41,113 @@ class PGMCompiler:
     @angle.setter
     def angle(self, angle):
         self._angle=angle
-        self.compute_t_matrix()
-        
-    @property
-    def long_pause(self):
-        return self._long_pause
-    
-    @long_pause.setter
-    def long_pause(self, long_pause):
-        self._long_pause=long_pause
-        
-    @property
-    def short_pause(self):
-        return self._short_pause
-    
-    @short_pause.setter
-    def short_pause(self, short_pause):
-        self._short_pause=short_pause
+        self._compute_t_matrix()
     
     # Methods
-    def compute_t_matrix(self):
-        RM = np.array([[np.cos(self._angle), -np.sin(self._angle), 0],
-                       [np.sin(self._angle), np.cos(self._angle), 0],
+    def _compute_t_matrix(self):
+        RM = np.array([[np.cos(self.angle), -np.sin(self.angle), 0],
+                       [np.sin(self.angle), np.cos(self.angle), 0],
                        [0, 0, 1]])
         SM = np.array([[1,0,0],
                        [0,1,0],
-                       [0,0,1/self._ind_rif]])
-        self._TM = np.dot(SM,RM)
+                       [0,0,1/self.ind_rif]])
+        self.TM = np.dot(SM,RM)
         
     def header(self):
-        if self._fab_line.lower() == 'capable':
-            self._instructions.append('ENABLE X Y Z\n')
-            self._instructions.append('METRIC\n')
-            self._instructions.append('SECONDS\n')
-            self._instructions.append('G359\n')
-            self._instructions.append('VELOCITY ON\n')
-            self._instructions.append('PSOCONTROL X RESET\n')
-            self._instructions.append('PSOOUTPUT X CONTROL 3 0\n')
-            self._instructions.append('PSOCONTROL X OFF\n')
-            self._instructions.append('ABSOLUTE\n')
-            self._instructions.append('G17\n')
-            self._instructions.append('DWELL 1\n')
-            self._instructions.append('\n')
-            self._instructions.append('\n')
-        elif self._fab_line.lower() == 'fire':
-            self._instructions.append('ENABLE X Y Z\n')
-            self._instructions.append('METRIC\n')
-            self._instructions.append('SECONDS\n')
-            self._instructions.append('WAIT MODE NOWAIT\n')
-            self._instructions.append('VELOCITY ON\n')
-            self._instructions.append('PSOCONTROL X RESET\n')
-            self._instructions.append('PSOCONTROL X OFF\n')
-            self._instructions.append('ABSOLUTE\n')
-            self._instructions.append('G17\n')
-            self._instructions.append('DWELL 1\n')
-            self._instructions.append('\n')
-            self._instructions.append('\n')
+        if self.fab_line.upper() == 'CAPABLE':
+            with open(os.path.join(CWD, 'header_capable.txt')) as fd:
+                self._instructions.extend(fd.readlines())
+        elif self.fab_line.upper() == 'FIRE':
+            with open(os.path.join(CWD, 'header_fire.txt')) as fd:
+                self._instructions.extend(fd.readlines())
     
     def comment(self, comstring):
         self._instructions.append(f'; {comstring}\n')
         
+    def shutter(self, state):
+        if state.upper() == 'ON' and self._shutter_on is False:
+            self._shutter_on = True
+            self._instructions.append('PSOCONTROL X ON\n')
+        elif state.upper() == 'OFF' and self._shutter_on is True:
+            self._shutter_on = False
+            self._instructions.append('PSOCONTROL X OFF\n')
+        else:
+            pass
+    
     def rpt(self, num):
         self._instructions.append(f'REPEAT {num}\n')
         
     def endrpt(self):
         self._instructions.append('ENDREPEAT\n\n')
     
+    def dwell(self, time):
+        self._instructions.append(f'DWELL {time}\n\n')
+        self.total_dwell_time += float(time)
+        
+    def set_home(self, home_pos):
+        assert self._shutter_on is False, 'Try to move with OPEN shutter.'
+        assert np.size(home_pos) == 3, f'Given final position is not valid. 3 values are required, {np.size(home_pos)} were given.'
+        
+        x,y,z = home_pos
+        args = self._format_args(x, y, z)
+        space = ' ' if len(args) > 0 else ''
+        self._instructions.append('G92' + space + args + '\n')
+        
+    def homing(self):
+        self.move_to([0,0,0])
+        
+    def _format_args(self, x=None, y=None, z=None, f=None):
+        args = []
+        if x is not None:
+            args.append('{0}{1:.{digits}f}'.format('X', x, digits=self.output_digits))
+        if y is not None:
+            args.append('{0}{1:.{digits}f}'.format('Y', y, digits=self.output_digits))
+        if z is not None:
+            args.append('{0}{1:.{digits}f}'.format('Z', z, digits=self.output_digits))
+        if f is not None:
+            args.append('{0}{1:.{digits}f}'.format('F', f, digits=self.output_digits))
+        args = ' '.join(args)
+        return args
+    
+    def move_to(self, position, speed_pos=50):
+        assert self._shutter_on is False, 'Try to move with OPEN shutter.'
+        assert np.size(position) == 3, f'Given final position is not valid. 3 values are required, {np.size(position)} were given.'
+        
+        x, y, z = position
+        args = self._format_args(x, y, z, speed_pos)
+        
+        self._instructions.append(f'LINEAR {args}\n')
+        self.dwell(self.long_pause)
+    
     def point_to_instruction(self, M):
-        x = M['x']; y = M['y']; z = M['z']; f = M['f']; s = M['s']
+        c = np.column_stack((M['x'],M['y'],M['z']))
+        c_rot = np.dot(self.TM, c.T).T
+        x,y,z = c_rot[:, 0],c_rot[:, 1],c_rot[:, 2]; f = M['f']; s = M['s']
         
-        coord = np.column_stack((x,y,z))
-        t_coord = np.dot(self._TM, coord.T).T
-        x, y, z = t_coord[:, 0], t_coord[:, 1], t_coord[:, 2]
-        
-        shutter_on = False
-        
-        for i in range(len(x)):
-            if s[i] == 0 and shutter_on is False:
-                # self._instructions += 'PSOCONTROL X OFF\n'
-                self._instructions.append(f'LINEAR X{x[i]:.6f} Y{y[i]:.6f} Z{z[i]:.6f} F{f[i]:.6f}\n')
-                self._instructions.append(f'DWELL {self._long_pause:.6f}\n\n')
-            elif s[i] == 0 and shutter_on is True:
-                self._instructions.append('PSOCONTROL X OFF\n')
-                self._instructions.append(f'DWELL {self._short_pause:.6f}\n')
-                shutter_on = False
-            elif s[i] == 1 and shutter_on is False:
-                self._instructions.append('PSOCONTROL X ON\n')
-                self._instructions.append(f'LINEAR X{x[i]:.6f} Y{y[i]:.6f} Z{z[i]:.6f} F{f[i]:.6f}\n')
-                shutter_on = True
+        for i in range(len(x)): 
+            args = self._format_args(x[i], y[i], z[i], f[i])
+            if s[i] == 0 and self._shutter_on is False:
+                self._instructions.append(f'LINEAR {args}\n')
+                self.dwell(self.long_pause)
+            elif s[i] == 0 and self._shutter_on is True:
+                self.shutter('OFF')
+                self.dwell(self.short_pause)
+            elif s[i] == 1 and self._shutter_on is False:
+                self.shutter('ON')
+                self._instructions.append(f'LINEAR {args}\n')
             else:
-                self._instructions.append(f'LINEAR X{x[i]:.6f} Y{y[i]:.6f} Z{z[i]:.6f} F{f[i]:.6f}\n')
+                self._instructions.append(f'LINEAR {args}\n')
         return (x,y,z,f,s)
     
     def compile_pgm(self):
-        if not self._filename.endswith('.pgm'):
-            self._filename += '.pgm'
         
-        f = open(self._filename, "w")
+        if self.filename is None:
+            return
+        
+        if not self.filename.endswith('.pgm'):
+            self.filename += '.pgm'
+        
+        f = open(self.filename, "w")
         f.write(''.join(self._instructions))
         f.close()
         print('G-code compilation completed.')
@@ -155,38 +155,32 @@ class PGMCompiler:
 if __name__ == '__main__':
     
     # Data
-    speed = 20
-    
-    radius = 15
     pitch = 0.080
-    depth = 0.035
     int_dist = 0.007
-    int_length = 0.0
-    angle = np.radians(45)
-    tot_length = 25
-    length_arm = 1.5
+    angle = np.radians(1)
+    ind_rif = 1.5/1.33
     
     d_bend = 0.5*(pitch-int_dist)
-    Dx = 4; Dy = 0.0; Dz = 0.0
-    increment = [Dx, Dy, Dz]
+    increment = [4,0,0]
     
     # Calculations
-    coup = [Waveguide(num_scan=6) for _ in range(20)]
+    coup = [Waveguide(num_scan=6) for _ in range(2)]
     for index, wg in enumerate(coup):
-        [xi, yi, zi] = [-2, -pitch/2 + index*pitch, depth]
-        
-        wg.start([xi, yi, zi])
-        wg.linear(increment, speed)
-        wg.sin_mzi((-1)**index*d_bend, radius, length_arm, speed,)
-        wg.linear(increment, speed)
+        wg.start([-2, -pitch/2 + index*pitch, 0.035])
+        wg.linear(increment, speed=20)
+        wg.sin_mzi((-1)**index*d_bend, radius=15, arm_length=1.0, speed=20, N=50)
+        wg.linear(increment, speed=20)
         wg.end()
     
     # Compilation 
-    gc = PGMCompiler('MZImultiscan', ind_rif=1.5/1.33, angle=angle)
+    gc = PGMCompiler('testPGMcompiler', ind_rif=ind_rif, angle=angle)
     gc.header()
     gc.rpt(wg.num_scan)
     for i, wg in enumerate(coup):    
         gc.comment(f'Modo: {i}')
         gc.point_to_instruction(wg.M)
     gc.endrpt()
+    gc.move_to([None,0,0.1])
+    gc.set_home([0,0,0])
+    gc.homing()
     gc.compile_pgm()
