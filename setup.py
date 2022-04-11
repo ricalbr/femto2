525156063f5055ccc97099cbe0ac2108c7d4084a
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-04-11 20:06:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='flww',
+    name='femto',
     description='Python suite for the design of femtosecond-written circuit.',
     version='1.0',
-    packages=find_packages(include=['flww',
+    packages=find_packages(include=['femto',
                                     'compiler',
                                     'compiler.*',
                                     'objects',
