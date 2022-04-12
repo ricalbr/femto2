786338d08c759530f1e350d96876714af57e04f0
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-04-12 11:04:29 +0200

    Use dict() to organize circuit

--- examples/20X20/circ20x20.py
+++ examples/20X20/circ20x20.py
@@ -11,35 +11,35 @@ import time
 t = time.time()
 
 # 20x20 circuit
-circ20x20 = [Waveguide() for _ in range(p.MM)]
-marker = [Marker(1, 0.06) for _ in range(p.MM)]
+circ = {
+    'waveguide':    [Waveguide() for _ in range(p.MM)], 
+    'marker':   [Marker(p.lx, p.ly) for _ in range(p.MM)]
+    }
 
-increment = [p.swg_length, 0.0, 0.0]
-
-for i, wg in enumerate(circ20x20):
+for i, wg in enumerate(circ['waveguide']):
     [xi, yi, zi] = [p.x0, p.y0-(i-0.5*(p.MM+1))*p.pitch, p.depth]
 
     wg.start([xi, yi, zi])
-    wg.linear(increment, p.speed)
+    wg.linear(p.increment, p.speed)
     wg.sin_bend((-1)**(i%2+1)*p.d1, p.radius, p.speed)
     for j in range(p.NN-1):
         wg.sin_bend((-1)**(j+i%2)*p.d1, p.radius, speed=p.speed)
-        if i == p.NN-1: marker[j].cross([wg.M['x'].iloc[-1],wg.M['y'].iloc[-1]-.2,0.001])
+        if i == p.NN-1: circ['marker'][j].cross([wg.M['x'].iloc[-1],wg.M['y'].iloc[-1]-.2,0.001])
         wg.sin_bend((-1)**(j+i%2+1)*p.d1, p.radius, speed=p.speed)
         wg.sin_bend((-1)**(j+i%2)*p.d2, p.radius, p.speed)
     wg.sin_bend((-1)**(j+i%2+1)*p.d1, p.radius, p.speed)
-    if i == p.NN-1: marker[j+1].cross([wg.M['x'].iloc[-1],wg.M['y'].iloc[-1]-.2,0.001])
+    if i == p.NN-1: circ['marker'][j+1].cross([wg.M['x'].iloc[-1],wg.M['y'].iloc[-1]-.2,0.001])
     wg.sin_acc((-1)**(j+i%2)*p.d1, p.radius, speed=p.speed)
-    wg.linear(increment, p.speed)
+    wg.linear(p.increment, p.speed)
     wg.end()
 
 # Plot
 fig, ax = plt.subplots()
-for wg in circ20x20:
+for wg in circ['waveguide']:
     ax.plot(wg.M['x'][:-1], wg.M['y'][:-1], '-b', linewidth=2.5)
     ax.plot(wg.M['x'][-2:], wg.M['y'][-2:], ':k', linewidth=1)
     
-for c in marker:
+for c in circ['marker']:
     ax.plot(c.M['x'][:-1], c.M['y'][:-1], '-k', linewidth=1)
 plt.show()
 
@@ -48,7 +48,7 @@ plt.show()
 gc = PGMCompiler('20x20CIRC.pgm', ind_rif=p.ind_env, angle=p.angle)
 gc.header()
 gc.rpt(wg.num_scan)
-for i, wg in enumerate(circ20x20):
+for i, wg in enumerate(circ['waveguide']):
     gc.comment(f' +--- Modo: {i+1} ---+')
     gc.point_to_instruction(wg.M)
 gc.endrpt()
@@ -57,7 +57,7 @@ gc.compile_pgm()
 # # MARKERS
 gc = PGMCompiler('20x20MARKERS.pgm', ind_rif=p.ind_env, angle=p.angle)
 gc.header()
-for i, c in enumerate(marker):
+for i, c in enumerate(circ['marker']):
     gc.comment(f' +--- Croce: {i+1} ---+')
     gc.rpt(c.num_scan)
     gc.point_to_instruction(c.M)
