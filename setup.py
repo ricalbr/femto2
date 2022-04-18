1800d4a9e4e5607aae5083f4d4b707081748f0b0
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-04-18 22:44:12 +0200

    Add pyclipper as package requirement

--- setup.py
+++ setup.py
@@ -3,18 +3,19 @@ from setuptools import setup, find_packages
 setup(
     name='femto',
     description='Python suite for the design of femtosecond-written circuit.',
-    version='1.0',
+    version='1.1',
     packages=find_packages(include=['femto',
                                     'compiler',
                                     'compiler.*',
                                     'objects',
                                     'objects.*']),
     install_requires=[
-        'pandas',
-        'numpy',
         'matplotlib',
-        'plotly',
+        'numpy',
         'openpyxl',
-        'scipy'
+        'pandas',
+        'plotly',
+        'pyclipper',
+        'scipy',
     ],
 )
