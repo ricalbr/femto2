15a4e7806abdfb57b8b150b0e492be85a2be7057
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-05-11 00:09:15 +0200

    Add examples

---
+++ examples/MZIs+trench/mzi.py
@@ -0,0 +1,142 @@
+from femto import Waveguide, TrenchColumn, Marker, PGMCompiler
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# %% GEOMETRICAL DATA
+
+
+# Util function
+def get_sbend_par(D, R):
+    dy = np.abs(D/2)
+    a = np.arccos(1-(dy/R))
+    return 2*R*np.sin(a)
+
+
+# Circuit
+glass_height = 25
+glass_length = 25
+x_init = -2
+R = 15
+pitch = 0.080
+pitch_fa = 0.127
+depth = 0.035
+int_distance = 0.007
+int_length = 0.0
+length_arm = 0.0
+speed = 20
+swg_length = 3
+increment = [swg_length, 0.0, 0.0]
+N = 150
+
+x0 = -2.0
+y0 = 0.0
+z0 = depth
+
+ymzi = 0.08
+
+d1 = 0.5*(pitch-int_distance)
+d2 = pitch-int_distance
+
+# Markers
+lx = 1
+ly = 0.05
+
+# Trench
+lt = 1
+tspeed = 4
+nboxz = 4
+deltaz = 0.0015
+zoff = 0.02
+hbox = 0.075
+nrpt = np.ceil((hbox+zoff)/deltaz)
+speed_pos = 5
+base_folder = r'C:\Users\Capable\Desktop'
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+# %% G-CODE DATA
+n_scan = 6
+ind_glass = 1.5
+ind_water = 1.33
+ind_env = ind_glass/ind_water
+angle = 0.0
+
+# 20x20 circuit
+circ = {
+    'waveguide': [],
+    'marker': [],
+    'trench': [],
+    }
+
+# Guida dritta
+wg = Waveguide()
+wg.start([x0, y0, z0])
+wg.linear([glass_length+4, 0.0, 0.0], speed=speed)
+wg.end()
+circ['waveguide'].append(wg)
+
+# MZI
+delta_x = get_sbend_par(d1, R)
+l_x = (glass_length + 4 - delta_x*4)/2
+for i in range(6):
+    wg = Waveguide()
+    wg.start([x0, ymzi+i*pitch, z0])
+    wg.linear([l_x, 0, 0], speed=speed)
+    wg.arc_mzi((-1)**(i)*d1, R, speed=speed)
+    wg.linear([l_x, 0, 0], speed=speed)
+    wg.end()
+    circ['waveguide'].append(wg)
+
+# # Marker
+pos = [glass_length/2, y0-pitch]
+c = Marker()
+c.cross(pos, ly=0.1)
+circ['marker'].append(c)
+
+# # Trench
+col = TrenchColumn(x_c=glass_length/2,
+                   y_min=ymzi,
+                   y_max=ymzi+6*pitch-0.02)
+col.get_trench(circ['waveguide'])
+circ['trench'].append(col)
+
+# Plot
+fig, ax = plt.subplots()
+for wg in circ['waveguide']:
+    ax.plot(wg.x[:-1], wg.y[:-1], '-b', alpha=0.5, linewidth=0.5)
+
+for c in circ['marker']:
+    ax.plot(c.x[:-1], c.y[:-1], '-k', linewidth=1.25)
+
+for col in circ['trench']:
+    for t in col.trench_list:
+        patch = t.patch
+        ax.add_patch(patch)
+plt.tight_layout(pad=0)
+ax.set_aspect(10)
+plt.show()
+
+# Waveguide G-Code
+with PGMCompiler('MZIs.pgm', ind_rif=ind_env, angle=angle) as gc:
+    gc.rpt(n_scan)
+    for wg in circ['waveguide']:
+        gc.comment(f' +--- Modo: {i+1} ---+')
+        gc.point_to_instruction(wg.M)
+    gc.endrpt()
+    gc.homing()
+
+# Waveguide G-Code
+with PGMCompiler('Markers.pgm', ind_rif=ind_env, angle=angle) as gc:
+    for wg in circ['marker']:
+        gc.point_to_instruction(wg.M)
+    gc.homing()
+
+# Trench G-Code
+for col_index, col in enumerate(circ['trench']):
+    # Generate G-Code for the column
+    col_filename = os.path.join(os.getcwd(),
+                                's-trench',
+                                f'FARCALL{col_index:03}')
+    with PGMCompiler(col_filename, ind_rif=ind_env, angle=angle) as gc:
+        gc.make_trench(col, col_index, base_folder=base_folder)
+        gc.homing()
---
+++ examples/MxN UPP/circMxN.py
@@ -0,0 +1,92 @@
+from femto import Waveguide, TrenchColumn, Marker, PGMCompiler
+import matplotlib.pyplot as plt
+import param as p
+import time
+import os
+
+t0 = time.perf_counter()
+
+# MxN circuit
+circ = {
+    'waveguide': [Waveguide() for _ in range(p.MM)],
+    'marker': [Marker() for _ in range(p.NN)],
+    'trench': [TrenchColumn() for _ in range(p.NN)]
+    }
+
+x_trench = []
+for i, wg in enumerate(circ['waveguide']):
+    [xi, yi, zi] = [p.x0, p.y0+(i-0.5*(p.MM-1))*p.pitch, p.depth]
+
+    wg.start([xi, yi, zi])
+    wg.linear(p.increment, speed=p.speed)
+    wg.sin_bend((-1)**(i % 2)*p.d1, p.radius, speed=p.speed, N=p.N)
+    for j in range(p.NN-1):
+        wg.sin_bend((-1)**(j+i % 2+1)*p.d1, p.radius, speed=p.speed, N=p.N)
+        if i == 0:
+            xl, yl, _ = wg.lastpt
+            circ['marker'][j].cross([xl, yl-0.2], p.lx, p.ly)
+            x_trench.append(xl)
+        wg.sin_bend((-1)**(j+i % 2)*p.d1, p.radius, speed=p.speed, N=p.N)
+        wg.sin_bend((-1)**(j+i % 2+1)*p.d2, p.radius, speed=p.speed, N=p.N)
+    wg.sin_bend((-1)**(j+i % 2)*p.d1, p.radius, speed=p.speed, N=p.N)
+    if i == 0:
+        xl, yl, _ = wg.lastpt
+        circ['marker'][j+1].cross([xl, yl-0.2], p.lx, p.ly)
+        x_trench.append(xl)
+    wg.sin_acc((-1)**(j+i % 2+1)*p.d1, p.radius, speed=p.speed, N=p.N)
+    wg.linear(p.increment, speed=p.speed)
+    wg.end()
+
+# Trench
+for xt, col in zip(x_trench, circ['trench']):
+    col.x_c = xt
+    col.y_min = p.y0 - 0.5*p.MM*p.pitch
+    col.y_max = p.y0 + 0.5*p.MM*p.pitch
+    col.get_trench(circ['waveguide'])
+
+# Plot
+fig, ax = plt.subplots()
+for wg in circ['waveguide']:
+    ax.plot(wg.x[:-1], wg.y[:-1], '-b', linewidth=0.5) # shutter on
+    ax.plot(wg.x[-2:], wg.y[-2:], ':k', linewidth=0.5) # shutter off
+
+for c in circ['marker']:
+    ax.plot(c.x[:-1], c.y[:-1], '-k', linewidth=1.25)
+
+for col in circ['trench']:
+    for t in col.trench_list:
+        ax.add_patch(t.patch)
+plt.tight_layout(pad=0)
+plt.show()
+
+# Compilation
+# # OPTICAL CIRCUIT
+with PGMCompiler(f'{p.MM}x{p.NN}CIRC.pgm',
+                 ind_rif=p.ind_env,
+                 angle=p.angle) as gc:
+    gc.rpt(circ['waveguide'][0].num_scan)
+    for i, wg in enumerate(circ['waveguide']):
+        gc.comment(f' +--- Modo: {i+1} ---+')
+        gc.point_to_instruction(wg.M)
+    gc.endrpt()
+
+# # MARKERS
+with PGMCompiler(f'{p.MM}x{p.NN}MARKERS.pgm',
+                 ind_rif=p.ind_env,
+                 angle=p.angle) as gc:
+    for i, c in enumerate(circ['marker']):
+        gc.comment(f' +--- Croce: {i+1} ---+')
+        gc.rpt(c.num_scan)
+        gc.point_to_instruction(c.M)
+        gc.endrpt()
+
+# # TRENCH
+for col_index, col in enumerate(circ['trench']):
+    col_filename = os.path.join(os.getcwd(),
+                                's-trench',
+                                f'FARCALL_COLONNA{col_index+1:03}')
+    with PGMCompiler(col_filename, ind_rif=p.ind_env, angle=p.angle) as gc:
+        gc.make_trench(col, col_index, base_folder=p.base_folder)
+        gc.homing()
+
+print(f'Elapsed time: {time.perf_counter() - t0:.2f} s.')
