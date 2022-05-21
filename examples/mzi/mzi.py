a67f072d8cef5a0fab27bab1653a66f707d1d2dd
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-05-22 00:10:11 +0200

    Update mzi example with new methods

--- examples/mzi/mzi.py
+++ examples/mzi/mzi.py
@@ -1,10 +1,11 @@
 from femto import Waveguide, PGMCompiler
 import matplotlib.pyplot as plt
 import param as p
+import numpy as np
+np.set_printoptions(formatter={'float': "\t{: 0.2f}".format})
 
 
 # Simple script for fabricate a MZI interferometer
-
 mzi = [Waveguide() for _ in range(2)]
 increment = [p.swg_length, 0.0, 0.0]
 
@@ -12,25 +13,24 @@ for i, wg in enumerate(mzi):
     [xi, yi, zi] = [p.x0, p.y0-(0.5-i)*p.pitch, p.depth]
 
     wg.start([xi, yi, zi])
-    wg.linear(increment, p.speed)
+    wg.linear(increment, speed=p.speed)
     wg.sin_mzi((-1)**i*p.d, p.radius, arm_length=p.length_arm, speed=p.speed)
-    wg.linear(increment, p.speed)
+    wg.linear(increment, speed=p.speed)
     wg.end()
 
-print(wg.M)
+print(wg.points)
 
 # Plot
 fig, ax = plt.subplots()
 for wg in mzi:
-    ax.plot(wg.x[:-1], wg.y[:-1], color='k', linewidth=2.5)
+    ax.plot(wg.x[:-1], wg.y[:-1], '-k', linewidth=2.5)  # shutter on
+    ax.plot(wg.x[-2:], wg.y[-2:], ':b', linewidth=1.5)  # shutter off
 plt.show()
 
 # Compilation
-gc = PGMCompiler('MZImultiscan.pgm', ind_rif=p.ind_rif)
-gc.header()
-gc.rpt(wg.num_scan)
-for i, wg in enumerate(mzi):
-    gc.comment(f'Modo: {i+1}')
-    gc.point_to_instruction(wg.M)
-gc.endrpt()
-gc.compile_pgm()
+with PGMCompiler('MZImultiscan.pgm', ind_rif=p.ind_rif) as gc:
+    gc.repeat(wg.num_scan)
+    for i, wg in enumerate(mzi):
+        gc.comment(f'Modo: {i+1}')
+        gc.write(wg.points)
+    gc.end_repeat()
