f2472e4bbb6a203dc4c8bf61afc6881b933c2272
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-06-14 22:07:57 +0200

    Add helper functions
    pairwise() function is updated and a previous bug is fixed. The former implementation was selecting the elements (s0, s1, s2, s3, ...) as follows: (s0, s1), (s1, s2), (s2, s3), ....
    This repetition of elements is unwanted, in general.
    The new implementation is a particular case of a generic grouped() function that gruops an iterable in sets of n elements (n given by user).
    
    A swap function is also added to swap elements of an array given the index. The function is useful for waveguides that have a (odd number of) 3D crossing.
    
    Finally, a listcast function is added that convert any object into a list.

--- femto/helpers.py
+++ femto/helpers.py
@@ -1,7 +1,37 @@
-from itertools import tee
+from functools import partial
+from typing import List
 
 
-def pairwise(iterable):
-    a, b = tee(iterable)
-    next(b, None)
-    return zip(a, b)
+def grouped(iterable, n):
+    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
+    return zip(*[iter(iterable)] * n)
+
+
+# s -> (s0,s1), (s2,s3), (s3, s4), ...
+pairwise = partial(grouped, n=2)
+
+
+def swap(array, swap_pos: List[tuple], zero_index=False):
+    # in case of a single swap, swap_pos can be (pos1, pos2).
+    # Encapsulate the tuple in a list to have compatibility with general code
+    if not isinstance(swap_pos, list):
+        swap_pos = [swap_pos]
+
+    for pos1, pos2 in swap_pos:
+        if zero_index is False:
+            pos1 -= 1
+            pos2 -= 1
+        array[pos1], array[pos2] = array[pos2], array[pos1]
+    return array
+
+
+def listcast(x):
+    # cast any obj to a list
+    if isinstance(x, list):
+        return x
+    elif isinstance(x, str):
+        return [x]
+    try:
+        return list(x)
+    except TypeError:
+        return [x]
