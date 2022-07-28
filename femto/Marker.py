18b89af99cd2d876f370f89396f279ee8e20536f
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-07-28 16:59:04 +0200

    Add meander and ablation methods
    The following methods have been added:
    - meander to create a zig-zag meander pattern of a given width and given distance (delta) between parallel lines. The pattern joins an initial and final points that are given as input by the user
    
    - ablation create an ablation line given a list of point. Opitonally, the line can be displaced along x- and y-direction if a shift is given (default is None).

--- femto/Marker.py
+++ femto/Marker.py
@@ -17,75 +17,6 @@ class Marker(Waveguide):
     def __init__(self, param: dict):
         super().__init__(param)
 
-    def cross(self, position: List[float], lx: float = 1, ly: float = 0.05, speedpos: float = None):
-        """
-        Computes the points of a cross marker of given widht along x- and y-direction.
-
-        :param position: 2D ordered coordinate list that specifies the cross position [mm].
-            position[0] -> X
-            position[1] -> Y
-        :type position: List[float]
-        :param lx: Length of the cross marker along x [mm]. The default is 1 mm.
-        :type lx: float
-        :param ly: Length of the cross marker along y [mm]. The default is 0.05 mm.
-        :type ly: float
-        :param speedpos: Transition speed with the shutter closes [mm/s]. The default is self.speed.
-        :type speedpos: float
-        :return: None
-        """
-        if len(position) == 2:
-            position.append(self.depth)
-        elif len(position) == 3:
-            position[2] = self.depth
-            warnings.warn(f'Given 3D coordinate list. Z-coordinate is overwritten to {self.depth} mm.')
-        else:
-            raise ValueError('Given invalid position.')
-
-        if speedpos is None:
-            speedpos = self.speed_closed
-
-        start_pos = np.add(position, [-lx / 2, 0, 0])
-        self.start(start_pos, speedpos=5.0) \
-            .linear([lx, 0, 0], speed=self.speed) \
-            .linear([-lx / 2, -ly / 2, 0], speed=speedpos, shutter=0) \
-            .linear([0, 0, 0], speed=speedpos, shutter=1) \
-            .linear([0, ly, 0], speed=self.speed)
-        self.end()
-
-    def ruler(self, y_ticks: List, lx: float, lx_short: float = None, x_init: float = -2, speedpos: float = None):
-        """
-        Computes the points of a ruler marker. The y-coordinates of the ticks are specified by the user as well as
-        the length of the ticks in the x-direction.
-
-        :param y_ticks: y-coordinates of the ruler's ticks [mm]
-        :type y_ticks: List[float]
-        :param lx: Long tick length along x [mm]. The default is 1 mm.
-        :type lx: float
-        :param lx_short: Short tick length along x [mm]. The default is 0.75 mm.
-        :type lx_short: float
-        :param x_init: Starting x-coordinate of the laser [mm]. The default is -2 mm.
-        :type x_init: float
-        :param speedpos: Transition speed with the shutter closes [mm/s]. The default is self.speed.
-        :type speedpos: float
-        :return: None
-        """
-
-        if speedpos is None:
-            speedpos = self.speed_closed
-
-        if lx_short is None:
-            lx_short = 0.75 * lx
-        tick_len = lx_short * np.ones_like(y_ticks)
-        tick_len[0] = lx
-
-        self.start([x_init, y_ticks[0], self.depth])
-        for y, tlen in zip(y_ticks, tick_len):
-            self.linear([x_init, y, self.depth], speed=speedpos, mode='ABS', shutter=0)
-            self.linear([0, 0, 0], shutter=1)
-            self.linear([tlen, 0, 0], speed=self.speed, shutter=1)
-            self.linear([0, 0, 0], speed=self.speed, shutter=0)
-        self.end()
-
     def start(self, init_pos: List[float], speedpos: float = None) -> Self:
         """
         Starts a waveguide in the initial position given as input.
@@ -137,6 +68,133 @@ class Marker(Waveguide):
         self.add_path(x, y, z, f, s)
         self.fabrication_time()
 
+    def cross(self, position: List[float], lx: float = 1, ly: float = 0.05, speedpos: float = None):
+        """
+        Computes the points of a cross marker of given widht along x- and y-direction.
+
+        :param position: 2D ordered coordinate list that specifies the cross position [mm].
+            position[0] -> X
+            position[1] -> Y
+        :type position: List[float]
+        :param lx: Length of the cross marker along x [mm]. The default is 1 mm.
+        :type lx: float
+        :param ly: Length of the cross marker along y [mm]. The default is 0.05 mm.
+        :type ly: float
+        :param speedpos: Transition speed with the shutter closes [mm/s]. The default is self.speed.
+        :type speedpos: float
+        :return: None
+        """
+        if len(position) == 2:
+            position.append(self.depth)
+        elif len(position) == 3:
+            position[2] = self.depth
+            warnings.warn(f'Given 3D coordinate list. Z-coordinate is overwritten to {self.depth} mm.')
+        else:
+            raise ValueError('Given invalid position.')
+
+        if speedpos is None:
+            speedpos = self.speed_closed
+
+        start_pos = np.add(position, [-lx / 2, 0, 0])
+        self.start(start_pos, speedpos=5.0)
+        self.linear([lx, 0, 0], speed=self.speed)
+        self.linear([-lx / 2, -ly / 2, 0], speed=speedpos, shutter=0)
+        self.linear([0, 0, 0], speed=speedpos, shutter=1)
+        self.linear([0, ly, 0], speed=self.speed)
+        self.end()
+
+    def ruler(self, y_ticks: List, lx: float, lx_short: float = None, x_init: float = -2, speedpos: float = None):
+        """
+        Computes the points of a ruler marker. The y-coordinates of the ticks are specified by the user as well as
+        the length of the ticks in the x-direction.
+
+        :param y_ticks: y-coordinates of the ruler's ticks [mm]
+        :type y_ticks: List[float]
+        :param lx: Long tick length along x [mm]. The default is 1 mm.
+        :type lx: float
+        :param lx_short: Short tick length along x [mm]. The default is 0.75 mm.
+        :type lx_short: float
+        :param x_init: Starting x-coordinate of the laser [mm]. The default is -2 mm.
+        :type x_init: float
+        :param speedpos: Transition speed with the shutter closes [mm/s]. The default is self.speed.
+        :type speedpos: float
+        :return: None
+        """
+
+        if speedpos is None:
+            speedpos = self.speed_closed
+
+        if lx_short is None:
+            lx_short = 0.75 * lx
+        tick_len = lx_short * np.ones_like(y_ticks)
+        tick_len[0] = lx
+
+        self.start([x_init, y_ticks[0], self.depth])
+        for y, tlen in zip(y_ticks, tick_len):
+            self.linear([x_init, y, self.depth], speed=speedpos, mode='ABS', shutter=0)
+            self.linear([0, 0, 0], shutter=1)
+            self.linear([tlen, 0, 0], speed=self.speed, shutter=1)
+            self.linear([0, 0, 0], speed=self.speed, shutter=0)
+        self.end()
+
+    def meander(self, init_pos: List[float], final_pos: List[float], width: float = 1, delta: float = 0.001,
+                orientation: str = 'x', speedpos: float = None):
+
+        if speedpos is None:
+            speedpos = self.speed_closed
+
+        if orientation.lower() not in ['x', 'y']:
+            raise ValueError('Orientation must be either "x" (parallel lines along x) or "y" (parallel lines along y).'
+                             f'Given {orientation}.')
+        elif orientation.lower() == 'x':
+            num_passes = int(np.abs(init_pos[1] - final_pos[1])/delta)
+            delta = np.sign(final_pos[1] - init_pos[1]) * delta
+
+            self.start(init_pos, speedpos=5.0)
+            for i, _ in enumerate(range(num_passes)):
+                self.linear([(-1)**i*width, 0, 0], mode='INC')
+                self.linear([0,delta,0], mode='INC')
+            self.linear([(-1)**(i+1)*width, 0, 0], mode='INC')
+            self.end()
+
+        else:
+            num_passes = int(np.abs(init_pos[0] - final_pos[0])/delta)
+            delta = np.sign(final_pos[0] - init_pos[0]) * delta
+
+            self.start(init_pos, speedpos=5.0)
+            for i, _ in enumerate(range(num_passes)):
+                self.linear([self.lastx, (-1)**i*width, self.lastz], mode='ABS')
+                self.linear([delta,0,0], mode='INC')
+            self.linear([(-1)**i*width, self.lasty, self.lastz], mode='ABS')
+            self.end()
+
+    def ablation(self, points: List[List[float]] = None, shift: float = None, speedpos: float = None):
+        if points is None: return
+
+        if speedpos is None:
+            speedpos = self.speed_closed
+
+        self.start(points.pop(0))
+        for p in points:
+            if p[0] == None: p[0] = self.lastx
+            if p[1] == None: p[1] = self.lasty
+            if p[2] == None: p[2] = self.lastz
+            self.linear(p, mode='ABS')
+
+        if shift is not None:
+            points = np.asarray(points)
+            shifted_points = []
+            shifted_points.append(np.add(points, [shift, 0, 0]))
+            shifted_points.append(np.add(points, [-shift, 0, 0]))
+            shifted_points.append(np.add(points, [0, shift, 0]))
+            shifted_points.append(np.add(points, [0, -shift, 0]))
+
+            for shift in shifted_points:
+                self.linear(shift[0], mode='ABS', shutter=0)
+                for p in shift:
+                    self.linear(p, mode='ABS')
+                self.linear(shift[-1], mode='ABS', shutter=0)
+        self.end()
 
 def _example():
     from femto import PGMCompiler
