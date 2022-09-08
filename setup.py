a8a7083d7f31fb4dd64d9bed33ab771b03f58251
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-09-08 23:13:13 +0200

    Format and update of setup file

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name='femto',
-    description='Python suite for the design of femtosecond-written circuit.',
-    version='1.1',
-    packages=find_packages(include=['femto',
-                                    'compiler',
-                                    'compiler.*',
-                                    'objects',
-                                    'objects.*']),
-    install_requires=[
-        'matplotlib',
-        'numpy',
-        'pandas',
-        'plotly',
-        'scipy',
-    ],
+        name='femto',
+        description='Python suite for the design of femtosecond laser-written circuit.',
+        version='1.2',
+        packages=find_packages(include=['femto',
+                                        'compiler',
+                                        'compiler.*',
+                                        'objects',
+                                        'objects.*']),
+        install_requires=[
+                'matplotlib',
+                'numpy',
+                'pandas',
+                'plotly',
+                'scipy',
+        ],
 )
