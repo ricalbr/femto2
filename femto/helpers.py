3af31c9529cb760af1baadd752643d7f946e2dc4
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-10-08 14:40:35 +0200

    Add sign utility
    Use generator to alternate between 1 and -1.

--- femto/helpers.py
+++ femto/helpers.py
@@ -1,4 +1,5 @@
 from functools import partial
+from itertools import cycle
 from typing import List
 
 
@@ -61,3 +62,7 @@ def flatten(items, seqtypes=(list, tuple)):
     except IndexError:
         pass
     return items
+
+
+def sign():
+    return cycle([1, -1])
