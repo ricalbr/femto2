a348be12446f0913cf2af0dc4beb7817f1b433dc
Riccardo Albiero <albr.riccardo@gmail.com>
2022-11-06 15:47:32 +0100

    Create test_waveguide.py

---
+++ tests/test_waveguide.py
@@ -0,0 +1,99 @@
+import pytest
+
+from src.femto.Waveguide import _Waveguide
+
+
+@pytest.fixture
+def param() -> dict:
+    p = {
+            'scan'        : 6,
+            'speed'       : 20.0,
+            'y_init'      : 1.5,
+            'z_init'      : 0.050,
+            'speed_closed': 75,
+            'samplesize'  : (100, 15),
+            'depth'       : 0.035,
+            'radius'      : 25,
+            'pitch'       : 0.127,
+            'int_dist'    : 0.005,
+            'int_length'  : 0.0,
+            'arm_length'  : 1.0,
+            'ltrench'     : 1.5,
+            'dz_bridge'   : 0.006,
+    }
+    return p
+
+
+@pytest.fixture
+def empty_path(param) -> _Waveguide:
+    return _Waveguide(**param)
+
+
+@pytest.fixture
+def waveguide(param) -> _Waveguide:
+    # create LaserPath instance
+    wg = _Waveguide(**param)
+
+    # add path
+    pass
+
+    return wg
+
+
+def test_default_values() -> None:
+    wg = _Waveguide()
+    assert wg.scan == int(1)
+    assert wg.speed == float(1.0)
+    assert wg.x_init == float(-2.0)
+    assert wg.y_init == float(0.0)
+    assert wg.z_init == float(0.035)
+    assert wg.lsafe == float(2.0)
+    assert wg.speed_closed == float(5.0)
+    assert wg.speed_pos == float(0.5)
+    assert wg.cmd_rate_max == int(1200)
+    assert wg.acc_max == int(500)
+    assert wg.samplesize == (None, None)
+    assert wg.depth == float(0.035)
+    assert wg.radius == float(15)
+    assert wg.pitch == float(0.080)
+    assert wg.pitch_fa == float(0.127)
+    assert wg.int_dist is None
+    assert wg.int_length == float(0.0)
+    assert wg.arm_length == float(0.0)
+    assert wg.ltrench == float(1.0)
+    assert wg.dz_bridge == float(0.007)
+    assert wg.margin == float(1.0)
+
+
+def test_wg_values(waveguide) -> None:
+    assert waveguide.scan == int(6)
+    assert waveguide.speed == float(20.0)
+    assert waveguide.x_init == float(-2.0)
+    assert waveguide.y_init == float(1.5)
+    assert waveguide.z_init == float(0.050)
+    assert waveguide.lsafe == float(2.0)
+    assert waveguide.speed_closed == float(75)
+    assert waveguide.speed_pos == float(0.5)
+    assert waveguide.cmd_rate_max == int(1200)
+    assert waveguide.acc_max == int(500)
+    assert waveguide.samplesize == (100, 15)
+    assert waveguide.depth == float(0.035)
+    assert waveguide.radius == float(25)
+    assert waveguide.pitch == float(0.127)
+    assert waveguide.pitch_fa == float(0.127)
+    assert waveguide.int_dist == float(0.005)
+    assert waveguide.int_length == float(0.0)
+    assert waveguide.arm_length == float(1.0)
+    assert waveguide.ltrench == float(1.5)
+    assert waveguide.dz_bridge == float(0.006)
+    assert waveguide.margin == float(1.0)
+
+
+def test_z_init():
+    wg = _Waveguide(z_init=None, depth=0.05)
+    assert wg.z_init == float(0.05)
+
+
+def test_scan():
+    with pytest.raises(ValueError):
+        wg = _Waveguide(scan=1.2)
