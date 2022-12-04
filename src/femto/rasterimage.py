7a20eddab1568204c2b9754db9581a47043138f7
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2022-12-04 15:23:03 +0100

    Refactor RasterImage class

--- src/femto/rasterimage.py
+++ src/femto/rasterimage.py
@@ -4,148 +4,96 @@ from dataclasses import dataclass
 
 import numpy as np
 from femto.helpers import dotdict
+from femto.helpers import split_mask
 from femto.laserpath import LaserPath
-from femto.utils.GCODE_plot_colored import GCODE_plot_colored
 from PIL import Image
 
 
 @dataclass(repr=False)
 class RasterImage(LaserPath):
     """
-    Class representing an X raster laser path in the Xy plane obtained from a balck and white image.
+    Class representing an raster laser path in the xy-plane from a balck and white image.
     """
 
-    px_to_mm: float = 0.01  # pixel to millimeter scale when converting image to laser path
+    px_to_mm: float = 0.01  # pixel to millimeter scale convertion
     img_size: tuple[int, int] = (0, 0)
 
     def __post_init__(self):
         super().__post_init__()
 
-    def __repr__(self):
-        return f'{self.__class__.__name__}@{id(self) & 0xFFFFFF:x}'
-
     @property
-    def path_size(self):
-        if not all(self.img_size):  # check if img_size is None
-            raise ValueError('No image size given, unable to compute laserpath dimension')
+    def path_size(self) -> list[float]:
+        if not all(self.img_size):  # check if img_size is non-zero
+            raise ValueError('No image size given, unable to compute laserpath dimension.')
         else:
-            return tuple(self.px_to_mm * elem for elem in self.img_size)
+            return [self.px_to_mm * elem for elem in self.img_size]
 
     # Methods
-    def convert_image_to_path(self, img, display_flag=False):
+    def image_to_path(self, img: Image.Image, show: bool = False) -> None:
         # displaing image information
-        print('Image opened. Displaying information')
-        print(img.format)
-        print(img.size)
-        print(img.mode)
-        print('----------')
-
         self.img_size = img.size  # update of img_size property
-
-        print(f'Laser path dimension {self.path_size[0]:.3f} by {self.path_size[1]:.3f} mm^2')
-        print('----------')
+        print('Image opened. Displaying information..')
+        print(f'Extension:\t{img.format}\nImage size:\t{img.size}\nColor mode:\t{img.mode}\n', '-' * 40)
+        print(f'Laser path dimension {self.path_size[0]:.3f} by {self.path_size[1]:.3f} mm^2\n', '-' * 40)
 
         if img.mode != '1':
-            print(
-                'The program takes as input black and white images. Conversion of input image to BW with arbitrary '
-                'threshold at half of scale'
-            )
-            img_BW = img.convert('1', dither=None)
-            if display_flag:
-                img_BW.show()
+            img = img.convert('1')
+        if show:
+            img.show()
+        img_matrix = np.asarray(img, dtype=bool)
 
-        data = np.asarray(img_BW)
-        GCODE_array = np.array([0, 0, 0, 2, 0, 0.1, 0, 0])  # initialization  of the GCODE array
+        x_scan = np.arange(0, self.img_size[0] * self.px_to_mm, self.px_to_mm)
+        y_scan = np.arange(0, self.img_size[1] * self.px_to_mm, self.px_to_mm)
+        z_val = self.z_init or 0.0
 
-        for ii in range(data.shape[0]):
-            pixel_line = data[ii, :]
-            pixel_line = np.append(pixel_line, False)  # appending a final balck pixel, to ensure that the shutter is
-            # closed at the end
-            pixel_line_shifted = np.zeros(pixel_line.size)
-            pixel_line_shifted[1:] = pixel_line[0:-1]
-            shutter_switch_array = pixel_line - pixel_line_shifted
+        # loop through all the rows of the image
+        # TODO: chiedi a fede come ruotare la matrice (mettiamo una flag?)
+        for row, y_val in zip(img_matrix, y_scan):
+            x_open_shutter = split_mask(x_scan, ~row)
 
-            new_GCODE_line = np.array(
-                [-1 * self.px_to_mm, (ii - 1) * self.px_to_mm, 0, self.speed_closed, 0, 0.5, 0, 0]
-            )
-            # first move with closed shutter
-            GCODE_array = np.vstack([GCODE_array, new_GCODE_line])
-            new_GCODE_line = np.array(
-                [-1 * self.px_to_mm, ii * self.px_to_mm, 0, self.speed_pos, 0, 0.5, 0, 0]
-            )  # first move with closed shutter
-            GCODE_array = np.vstack([GCODE_array, new_GCODE_line])
+            # continue if no points with open shutter are found
+            if not x_open_shutter:
+                continue
 
-            shutter_state = 0
-            speed = self.speed * 2
-            indeces_shutter_closure = np.where(shutter_switch_array == -1)[0]
-            if indeces_shutter_closure.size != 0:
-                if sum(abs(shutter_switch_array)) != 0:
-                    for jj in range(min(indeces_shutter_closure[-1], pixel_line.size) + 1):
-                        if shutter_switch_array[jj] == 1:
-                            new_GCODE_line = np.array(
-                                [
-                                    jj * self.px_to_mm,
-                                    ii * self.px_to_mm,
-                                    0,
-                                    speed,
-                                    shutter_state,
-                                    0.1,
-                                    0,
-                                    0,
-                                ]
-                            )
-                            GCODE_array = np.vstack([GCODE_array, new_GCODE_line])
-                            shutter_state = 1
-                            speed = self.speed
-                        elif shutter_switch_array[jj] == -1:
-                            new_GCODE_line = np.array(
-                                [
-                                    jj * self.px_to_mm,
-                                    ii * self.px_to_mm,
-                                    0,
-                                    speed,
-                                    shutter_state,
-                                    0.1,
-                                    0,
-                                    0,
-                                ]
-                            )
-                            GCODE_array = np.vstack([GCODE_array, new_GCODE_line])
-                            shutter_state = 0
-                            speed = self.speed * 2
-        self.add_path(
-            GCODE_array[:, 0],
-            GCODE_array[:, 1],
-            GCODE_array[:, 2],
-            GCODE_array[:, 3],
-            GCODE_array[:, 4],
-        )
-        return GCODE_array
+            # add a path for each sub-split with open shutter
+            for x_split in x_open_shutter:
+                x_row = np.array([x_split[0]] + list(x_split) + [x_split[-1]] + [x_split[0]], dtype=np.float32)
+                y_row = y_val * np.ones_like(x_row, dtype=np.float32)
+                z_row = z_val * np.ones_like(x_row, dtype=np.float32)
+                f_row = np.array(
+                    [self.speed_closed] + [self.speed] * len(x_split) + [self.speed_closed, self.speed_closed],
+                    dtype=np.float32,
+                )
+                s_row = np.array([0] + [1] * len(x_split) + [0, 0], dtype=int)
+
+                self.add_path(x_row, y_row, z_row, f_row, s_row)
 
 
-def _example():
-    from PIL import ImageDraw, ImageFont
+def main():
+    import matplotlib.pyplot as plt
 
-    img = Image.new('L', (512, 256), color=255)
-    font = ImageFont.truetype('arial.ttf', 40)
-    d = ImageDraw.Draw(img)
-    d.text((150, 100), 'Hello World', font=font, fill=0)
+    PARAM_RIMG = dotdict(px_to_mm=0.04, speed=1)
 
-    # img.show()
-    R_IMG_PARAMETERS = dotdict(
-        px_to_mm=0.04,
-        speed=1,
-    )
+    im = Image.open(r'.\\utils\\logo.png')
+    im.thumbnail((512, 512), Image.ANTIALIAS)
 
-    r_img = RasterImage(**R_IMG_PARAMETERS)
-    GCODE_array = r_img.convert_image_to_path(img)
+    r_img = RasterImage(**PARAM_RIMG)
+    r_img.image_to_path(im)
 
-    fig_colored = GCODE_plot_colored(GCODE_array)
-    fig_colored.show()
+    # Plot
+    fig, ax = plt.subplots()
+    x, y, *_, s = r_img.points
+    for x_seg, y_seg in zip(split_mask(x, s.astype(bool)), split_mask(y, s.astype(bool))):
+        ax.plot(x_seg, y_seg, '-k', linewidth=2.5)
+    for x_seg, y_seg in zip(split_mask(x, ~s.astype(bool)), split_mask(y, ~s.astype(bool))):
+        ax.plot(x_seg, y_seg, ':b', linewidth=0.5)
+    ax.set_xlim([0, 0.04 * im.size[0]])
+    ax.set_ylim([0, 0.04 * im.size[1]])
+    plt.show()
 
     print(f'Expected writing time {r_img.fabrication_time:.3f} seconds')
     print(f'Laser path length {r_img.length:.3f} mm')
 
 
 if __name__ == '__main__':
-    _example()
+    main()
