60024188fa48d3e3999ed35cf9beac6fcd2f16b2
Riccardo Albiero <albr.riccardo@gmail.com>
2023-10-25 14:37:09 +0200

    Update logger.py

--- src/femto/logger.py
+++ src/femto/logger.py
@@ -1,4 +1,4 @@
 from __future__ import annotations
 
-CONSOLEFORMATTER = '%(filename)16s: \t [%(levelname)8s] \t %(message)s'
+CONSOLEFORMATTER = '%(filename)16s:  [%(levelname)-.3s]  %(message)s'
 DEBUGFORMATTER = '[%(asctime)s] [%(levelname)s] (%(filename)s:%(lineno)s) \t- %(message)s'
