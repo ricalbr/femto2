b8fbcff26f7a7c48b2e0ae14b5451b99624c608a
Riccardo Albiero <45992199+ricalbr@users.noreply.github.com>
2023-12-08 00:36:41 +0100

    Add walklevel function
    A function that allows to traverse a directory tree down to a given level of depth.

--- src/femto/helpers.py
+++ src/femto/helpers.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import functools
 import itertools
+import os
 import pathlib
 import warnings
 from typing import Any
@@ -409,3 +410,28 @@ def lookahead(iterable):
 
     # Report the last value.
     yield next_item, True
+
+
+# TODO: test
+def walklevel(path, depth=1):
+    """Walklevel
+
+    It works just like os.walk, but you can pass it a level parameter that indicates how deep the recursion will go.
+    If depth is 1, the current directory is listed.
+    If depth is 0, nothing is returned.
+    If depth is -1 (or less than 0), the full depth is walked.
+    """
+    # If depth is negative, just walk
+    # Not using yield from for python2 compat and copy dirs to keep consistant behavior for depth = -1 and depth = inf
+    if depth < 0:
+        for root, dirs, files in os.walk(path):
+            yield root, dirs[:], files
+        return
+    elif depth == 0:
+        return
+    base_depth = path.rstrip(os.path.sep).count(os.path.sep)
+    for root, dirs, files in os.walk(path):
+        yield root, dirs[:], files
+        cur_depth = root.count(os.path.sep)
+        if base_depth + depth <= cur_depth:
+            del dirs[:]
