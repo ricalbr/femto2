316d3bfed445481ad9cdbfa512c0b78a5940f0df
ricalbr <albr.riccardo@gmail.com>
2025-12-24 19:12:30 +0100

    (chore) polish log messages

--- src/femto/logger.py
+++ src/femto/logger.py
@@ -1,38 +1,42 @@
 from __future__ import annotations
 
 import logging
-
-DEBUGFORMATTER = '[%(asctime)s] [%(levelname)s] (%(filename)s:%(lineno)s) \t- %(message)s'
+from datetime import datetime
 
 
 class CustomConsoleFormatter(logging.Formatter):
-    reset = '\x1b[0m'
-    bold = '\x1b[1m'
-    italic = '\x1b[3m'
-    reverse = '\x1b[7m'
+    # ANSI styles
+    RESET = "\x1b[0m"
+    BOLD = "\x1b[1m"
+    DIM = "\x1b[2m"
+    ITALIC = "\x1b[3m"
+    REVERSE = "\x1b[7m"
 
-    white = f'{reset}\x1b[38;2;255;255;255m'
-    bold_white = f'{reset}{bold}\x1b[38;2;255;255;255m'
-    yellow = f'{reset}\x1b[38;2;255;255;95m'
-    italic_yellow = f'{reset}{italic}\x1b[38;2;255;255;95m'
-    bold_yellow = f'{reset}{bold}\x1b[38;2;255;255;95m'
-    mintgreen = f'{reset}\x1b[38;2;153;255;153m'
-    bold_mintgreen = f'{reset}{bold}\x1b[38;2;153;255;153m'
-    red = f'{reset}\x1b[38;2;255;127;80m'
-    bold_red = f'{reset}{bold}\x1b[38;2;255;127;80m'
-    rev_bold_red = f'{reset}{reverse}{bold}\x1b[38;2;255;127;80m'
+    # Color palette
+    FG_WHITE = "\x1b[38;2;236;239;244m"
+    FG_GRAY = "\x1b[38;2;160;170;180m"
+    FG_BLUE = "\x1b[38;2;136;192;208m"
+    FG_GREEN = "\x1b[38;2;163;190;140m"
+    FG_YELLOW = "\x1b[38;2;235;203;139m"
+    FG_RED = "\x1b[38;2;191;97;106m"
+    FG_PURPLE = "\x1b[38;2;180;142;173m"
 
-    CONSOLEFORMATTER = '{}%(module)12s:  {}[%(levelname)-.3s]  {}%(message)s{}'
-
-    FORMATS = {
-        logging.DEBUG: CONSOLEFORMATTER.format(white, white, white, reset),
-        logging.INFO: CONSOLEFORMATTER.format(white, bold_mintgreen, white, reset),
-        logging.WARNING: CONSOLEFORMATTER.format(white, bold_yellow, italic_yellow, reset),
-        logging.ERROR: CONSOLEFORMATTER.format(white, bold_red, red, reset),
-        logging.CRITICAL: CONSOLEFORMATTER.format(white, rev_bold_red, rev_bold_red, reset),
+    LEVEL_STYLES = {
+        logging.DEBUG: FG_GRAY,
+        logging.INFO: FG_GREEN,
+        logging.WARNING: FG_YELLOW,
+        logging.ERROR: FG_RED,
+        logging.CRITICAL: BOLD + REVERSE + FG_RED,
     }
 
     def format(self, record: logging.LogRecord) -> str:
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
+        ts = datetime.fromtimestamp(record.created).strftime("%H:%M:%S")
+
+        level_style = self.LEVEL_STYLES.get(record.levelno, self.FG_WHITE)
+        level = f"{level_style}{record.levelname:<8}{self.RESET}"
+
+        module = f"{self.FG_BLUE}{record.module}{self.RESET}"
+        message = f"{self.FG_WHITE}{record.getMessage()}{self.RESET}"
+        timestamp = f"{self.DIM}{self.FG_GRAY}{ts}{self.RESET}"
+
+        return f"{timestamp}  {level}  {module} : {message}"
